07df57ebecfe2d963918cda392666fef44036798
Henrique Miranda <miranda.henrique@outlook.com.br>
2020-11-15 20:29:21 -0300

    Finished catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { makeRequest } from '../../core/utils/request';
+import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import { Link } from 'react-router-dom';
 import './styles.scss';
-import { ProductsResponse } from '../../core/types/Product';
+import { ProductsResponse } from 'core/types/Product';
 
-
-//quando o componente iniciar, buscar a lista de produtos
-
-//quando a lista de produtos estiver disponível, popular um estado no componente e
-//listar os produtos dinamicamente
 
 const Catalog = () => {
 
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
-
-    //console.log(productsResponse);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const params = {
@@ -23,8 +18,12 @@ const Catalog = () => {
             linesPerPage: 12
         };
 
+        setIsLoading(true);
         makeRequest({ url: '/products', params })
-            .then(response => setProductsResponse(response.data));
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            });
     }, []);
 
     return (
@@ -33,11 +32,13 @@ const Catalog = () => {
                 Catálogo de Produtos
             </h1>
             <div className="catalog-products">
-                {productsResponse?.content.map(product => (
-                    <Link to={`/products/${product.id}`} key={product.id}>
-                        <ProductCard product={product} />
-                    </Link>
-                ))}
+                {isLoading ? <ProductCardLoader /> : (
+                    productsResponse?.content.map(product => (
+                        <Link to={`/products/${product.id}`} key={product.id}>
+                            <ProductCard product={product} />
+                        </Link>
+                    ))
+                )}
             </div>
         </div>
     );
